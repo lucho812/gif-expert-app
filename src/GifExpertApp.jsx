99aab9b27d3b1188f6c7278b6efd310cb0488e82
lucho812 <luis.correa@ceiba.com.co>
2023-09-30 11:51:23 -0500

    Se quita archivo de barril

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import {AddCategory} from './components/AddCategory'
 import {GifGrid} from './components/GifGrid'
 
-
-
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['Dragon Ball']);
 
