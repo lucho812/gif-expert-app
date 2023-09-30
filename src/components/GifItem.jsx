94f40d0fcef99f658a80828a85c09cd7adef6e4c
lucho812 <luis.correa@ceiba.com.co>
2023-09-30 15:13:03 -0500

    Se coloca Key a GifItem

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,7 +1,7 @@
 
-export const GifItem = ({title, url}) => {
+export const GifItem = ({title, url, id}) => {
   return (
-    <div className="card">
+    <div className="card" key={id}>
         <img src={ url } alt= {title} />
         <p>{title}</p>
     </div>
